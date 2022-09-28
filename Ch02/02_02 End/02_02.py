4ce4587a1056d175cad5184b515d180fd82682e4
Yaxin(Janet) Zhuang <49347418+yqz5514@users.noreply.github.com>
2022-09-28 12:32:08 -0400

    u

--- Ch02/02_02 End/02_02.py
+++ Ch02/02_02 End/02_02.py
@@ -1,6 +1,6 @@
 >>> import numpy as np
 >>> import cv2
->>> img = cv2.imread("opencv-logo.png", 1)
+>>> img = cv2.imread("opencv-logo.png", 1)# 1 means want the origina color of picture, 0 indicates blacka  nd white
 >>> 
 >>> img
 array([[[255, 255, 255],
@@ -51,23 +51,25 @@ array([[[255, 255, 255],
         [255, 255, 255],
         [255, 255, 255],
         [255, 255, 255]]], dtype=uint8)
+# pixels
 >>> type(img)
 <class 'numpy.ndarray'>
 >>> len(img)
-739
+739 # number of horizontal rows
 >>> len(img[0])
-600
+600 # verical columns
 >>> len(img[0][0])
-3
+3 # channels found in pictures in this one is R,G, and B channel
 >>> img.shape
 (739, 600, 3)
 >>> img.dtype
-dtype('uint8')
->>> 2**8
-256
->>> img[10, 5]
-array([255, 255, 255], dtype=uint8)
->>> img[:, :, 0]
+dtype('uint8') # unassigned interger value of 8
+>>> 2**8 # a maxium of two to the eright values in each pixel
+256 # and the range of values vary from 0 to 255
+>>> img[10, 5] # 10th row, 5th column
+array([255, 255, 255], dtype=uint8) # pixel value at the array, given that our image has a 
+# maximm value of 255 of image, this is perfectly white
+>>> img[:, :, 0] # onw channel image
 array([[255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
@@ -75,5 +77,5 @@ array([[255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255],
        [255, 255, 255, ..., 255, 255, 255]], dtype=uint8)
->>> img.size
+>>> img.size # total number of pixels in image
 1330200
--- Ch02/02_03 End/02_03.py
+++ Ch02/02_03 End/02_03.py
@@ -1,13 +1,14 @@
 import numpy as np
 import cv2
 
-black = np.zeros([150,200,1],'uint8')
-cv2.imshow("Black",black)
+black = np.zeros([150,200,1],'uint8') # 150 pixels tall, 200 pixels width, 1 channel, and utin8(u:unsigned, int8: power of 8, this indicates the range of value allowed in this matrix will be 0 to 255) indicate type of image.
+cv2.imshow("Black",black)# show this image, window assigned black
 print(black[0,0,:])
 
 ones = np.ones([150,200,3],'uint8')
 cv2.imshow("Ones",ones)
 print(ones[0,0,:])
+
 
 white = np.ones([150,200,3],'uint16')
 white *= (2**16-1)
